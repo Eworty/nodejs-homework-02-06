cf220ca891f5c3b1e80328862906de30bbfed8e1
Pasha Piankovskyi <ghostangel21042017@gmail.com>
2023-07-01 18:26:11 +0300

    1.Connecting to MongoDB using Mongoose 2.Add patch(favorite)

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-const { DB_HOST } = process.env;
+
 const app = require("./app");
+
+const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
-    console.log("Database connect success");
   })
   .catch((error) => {
     console.log(error.message);
